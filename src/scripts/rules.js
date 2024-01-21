1fffa73373bb714e5acea8d00d11e15c1e756780
eullerAraujo <euller67@gmail.com>
2024-01-21 20:49:16 -0300

    Add aquirvos do jogo

---
+++ src/scripts/rules.js
@@ -0,0 +1,6 @@
+alert(`Olá Bem Vindo ao Jogo da Memória com Mojis!!😆
+\n Cada Par que você acerta irá ganha 10 pontos
+ \n Cada erro que você  tive perde 3 pontos
+ \n Se estiver pronto clique em "OK" para iniciar 
+ \n Divirta-se 😄.
+`)
